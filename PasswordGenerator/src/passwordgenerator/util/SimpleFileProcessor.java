3639c929821f00a6df93f334b75212ae576f4e9d
BMamba2942 <jmiddleton9140@sbcglobal.net>
2014-07-23 21:21:17 -0500

    Creates file when one isn't present.

--- PasswordGenerator/src/passwordgenerator/util/SimpleFileProcessor.java
+++ PasswordGenerator/src/passwordgenerator/util/SimpleFileProcessor.java
@@ -3,6 +3,8 @@ import java.io.File; //for File class
 import java.util.InputMismatchException; //for InputMismatchException
 import java.util.Scanner; //for Scanner class
 import java.util.ArrayList; //for LinkedList class
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 
 public class SimpleFileProcessor {
@@ -12,12 +14,14 @@ public class SimpleFileProcessor {
 		Scanner processName, //Scanners that process names
 		processEmail; //and emails, respectively
 		                                   
-		ArrayList<String> list, //Linked lists hold the raw data
+		ArrayList<String> list = null, //Linked lists hold the raw data
 		names, //data that is determined as a name
 		email; //data that is determined as an email
 		SimpleFileReader read; //Class that reads the file
 		//SimpleFileWriter writer; //Class that writes the file
 		String outName; //file name for the html output file
+                
+                Boolean fileExisted;
                 
                 /*
 		if(args.length < 1) //Check that there is an argument
@@ -28,31 +32,39 @@ public class SimpleFileProcessor {
 		}
                 */
                 
-                /*
+                
                 try
                 {
                     read = new SimpleFileReader(new File("testfile"));
                     list = read.processFile();
-                    names = new ArrayList<String>();
-                    email = new ArrayList<String>();
+                    names = new ArrayList<>();
+                    email = new ArrayList<>();
+                    fileExisted = true;
                 }
-                catch(Exception e)
+                catch(FileNotFoundException e)
                 {
-                    System.out.println("Exception " + e);
-                    e.printStackTrace();
-                }*/
+                    try 
+                    {
+                        FileWriter file = new FileWriter("testfile");
+                        file.close();
+                    }
+                    catch(Exception f){}
+                    fileExisted = false;
+                }
                 
                 
+                /*
 		read = new SimpleFileReader(new File("C:\\Users\\Joseph\\Documents\\NetBeansProjects\\PasswordGenerator\\src\\testfile"));
 		list = read.processFile();
 		names = new ArrayList<String>();
 		email = new ArrayList<String>();
+                */
                 
 		
 		
 		
                 
-                return list;
+                return fileExisted ? list : new ArrayList<String>();
         }
 		
 		//outName = args[0].substring(0, args[0].indexOf('.'));
