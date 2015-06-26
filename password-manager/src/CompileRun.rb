a805d88a671b0c59d30dd39855344a0e90af28a0
BMamba2942 <jmiddleton9140@sbcglobal.net>
2015-06-26 14:28:38 -0500

    Added Maven support

--- password-manager/src/CompileRun.rb
+++
@@ -1,8 +0,0 @@
-#Compile .java files
-for dir in Dir['passwordgenerator/*/'] do 
-	system("javac -d ../build/ " + dir + "*.java")
-end
-
-#Run application
-Dir.chdir('../build')
-system("java passwordgenerator/view/MainView")
